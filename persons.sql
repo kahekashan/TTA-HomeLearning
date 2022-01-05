a37e71c548183b39a30c447a9d820aaddd0b7db8
Kahekashan Nazeer <36095106+kahekashan@users.noreply.github.com>
2022-01-05 18:10:11 +0000

    Update persons.sql

--- persons.sql
+++ persons.sql
@@ -1,28 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 15, 2021 at 01:42 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `appjan`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `persons`
@@ -45,8 +20,3 @@ CREATE TABLE `persons` (
 --
 ALTER TABLE `persons`
   ADD PRIMARY KEY (`ID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
